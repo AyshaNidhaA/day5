0998123c86ec7dbc324815220279cdce1842d42e
AyshaNidhaA <ayshaaaa212@gmail.com>
2024-10-25 12:59:05 -0700

    first commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { RouterLink } from '@angular/router';
+import { SlicePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [SlicePipe,RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  constructor(private api: ApiService) {}
+  product: any = [ ]
+
+
+  ngOnInit() {
+    this.api.getData().subscribe((data: any) => {
+      this.product = data;
+    })
+  }
+
+}
---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export class ProductsComponent {
+
+constructor(public api: ApiService,private route: ActivatedRoute) {}
+  item: any;
+
+  ngOnInit() {
+
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.api.getDataById(id).subscribe((data) => {
+      this.item = data;
+      console.log(this.item);
+      
+
+
+
+  })
+}
+
+}
